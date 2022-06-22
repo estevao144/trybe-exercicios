e6c40f471883eb58700dd880bcf3bf9e1e4b998f
Estevao <estevo144@gmail.com>
2022-06-22 18:04:58 -0300

    criado pasta introducao

---
+++ Fundamentos/bl4-introducao-javascript-logica-prog/Dia2/script.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
---
+++ Fundamentos/bl4-introducao-javascript-logica-prog/dia1.1/questao8.js
@@ -0,0 +1,16 @@
+const a = 1;
+const b = 2;
+const c = 4;
+
+let par = false;
+
+if ((a % 2 = 0 || b % 2 = 0 || c % 2 = 0)){
+    par = true;
+}else{
+    
+    console.log(par);
+
+}
+
+
+    
