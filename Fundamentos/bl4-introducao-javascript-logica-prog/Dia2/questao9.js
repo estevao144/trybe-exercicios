a72c52ceacfe77bc12ddda2f34e7b8d143b6e0c4
Estevao <estevo144@gmail.com>
2022-06-22 20:45:42 -0300

    questao 9 resolvida

---
+++ Fundamentos/bl4-introducao-javascript-logica-prog/Dia2/questao9.js
@@ -0,0 +1,12 @@
+let numbers=[];
+
+
+for (let index = 0; index < 25; index++) {
+    numbers.push(index +1);
+    
+}
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]/2);
+    
+}
+
