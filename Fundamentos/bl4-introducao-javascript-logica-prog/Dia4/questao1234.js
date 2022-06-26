44f89750ac3a26fd5fc65f4160b25052b3107b29
Estevao <estevo144@gmail.com>
2022-06-26 18:21:17 -0300

    exercicios 6 ao 8 resolvidos.

---
+++ Fundamentos/bl4-introducao-javascript-logica-prog/Dia4/questao1234.js
@@ -0,0 +1,22 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+  console.log('bem vinda ', info.personagem);
+
+  for (const key in info) {
+   
+    console.log(key);
+  }
+
+  for (const key in info) {
+    
+    console.log(info[key]);
+   
+  }
+  //Questão de 1 a 4 feita.
+
+  
---
+++ Fundamentos/bl4-introducao-javascript-logica-prog/Dia4/script.js
@@ -0,0 +1,12 @@
+//exercicio de fixação:
+//1
+player = {
+    name : 'Marta',
+    lastName : 'Silva',
+    age : 34,
+    medals : { golden: 2, silver: 3 },
+};
+
+console.log('a jogadora ', player.name, ' ', player.lastName, ' tem ', player.age, 'anos de idade.' );
+
+console.log(player.medals[0]);
