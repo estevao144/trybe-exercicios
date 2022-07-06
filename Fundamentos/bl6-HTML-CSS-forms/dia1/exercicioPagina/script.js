8f85071c69d32ab979be080d2d4a80b757c27466
Estevao <estevo144@gmail.com>
2022-07-06 18:51:56 -0300

    exercicios bloco 6

---
+++ Fundamentos/bl6-HTML-CSS-forms/dia1/exercicioPagina/script.js
@@ -0,0 +1,15 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+//questão 1 
+HREF_LINK.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+});
+
+//questao 2 
+INPUT_CHECKBOX.addEventListener("click", (event)=>{
+event.preventDefault();
+});
